00a616c84725cb53fb1ea8cd16bcea1bb7930971
Ed1ezac <ed1ezac@gmail.com>
2023-04-27 07:28:29 +0200

    minor fixes

--- administrator/urls.py
+++ administrator/urls.py
@@ -14,7 +14,7 @@ from .views import (
 
 urlpatterns = [
     path('new/date', CreateTestDate.as_view(), name='new_date'),
-    path('create/groups', create_groups_and_perms, name='new_groups'),
+    path('refresh/groups', create_groups_and_perms, name='new_groups'),
     path('date/<int:pk>/edit', UpdateTestDate.as_view(), name='edit_date'),
     path('date/<int:pk>/delete', DeleteTestDate.as_view(), name='delete_date'),
     path('date/<int:pk>/details', TestDateDetails.as_view(), name='date_details'),
