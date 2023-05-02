d526fc36426f94495b50a7c2d81285424224cf23
Ed1ezac <ed1ezac@gmail.com>
2023-05-02 21:18:42 +0200

    adds user management and minor items

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-from .views import SignUpView, NotificationListView
+from .views import (
+        SignUpView,
+        showUsers,
+        to_client,
+        to_admin,
+        to_officer,
+        NotificationListView)
 
 urlpatterns = [
     path('register/', SignUpView.as_view(), name='register'),
     path('notifications/', NotificationListView.as_view(), name='notifications'),
+    path('manage/', showUsers.as_view(), name='man_users'),
+    path('<int:pk>/make-admin/', to_admin, name='mk_admin'),
+    path('<int:pk>/make-officer/', to_officer, name='mk_officer'),
+    path('<int:pk>/make-client/', to_client, name='mk_client'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
+from django.contrib import messages
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
 from django.contrib.auth.models import Group
 from django.views.generic import CreateView, ListView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 from .forms import CustomUserCreationForm
 from .models import Notification, CustomUser
@@ -22,6 +25,49 @@ class SignUpView(CreateView):
         self.object.progress = '1'
         self.object.save()
         return super(SignUpView, self).form_valid(form)  #save user
+
+
+class showUsers(LoginRequiredMixin, 
+        PermissionRequiredMixin, ListView):
+        model = CustomUser
+        template_name = 'admin/users.html'
+        permission_required = 'test_dates.delete_testdate'
+
+def can_manage_others(user):
+    return user.is_authenticated and user.has_perm("test_dates.delete_testdate")
+
+@user_passes_test(can_manage_others, login_url="/login")
+def to_admin(request, pk):
+    user = CustomUser.objects.get(id=pk)
+    #flush all groups
+    user.groups.clear()
+    grp, created = Group.objects.get_or_create(name="Administrator")
+    user.groups.add(grp)
+
+    messages.success(request, 'User has been granted Administrator privileges successfully.')
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/dates'))
+
+@user_passes_test(can_manage_others, login_url="/login")
+def to_officer(request, pk):
+    user = CustomUser.objects.get(id=pk)
+    #flush all groups 
+    user.groups.clear()
+    grp, created = Group.objects.get_or_create(name="Officer")
+    user.groups.add(grp)
+
+    messages.success(request, 'User has been granted Officer privileges successfully.')
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/dates'))
+
+@user_passes_test(can_manage_others, login_url="/login")
+def to_client(request, pk):
+    user = CustomUser.objects.get(id=pk)
+    #flush all groups 
+    user.groups.clear()
+    grp, created = Group.objects.get_or_create(name="Client")
+    user.groups.add(grp)
+
+    messages.success(request, 'User is now a client, privileges removed successfully.')
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/dates'))
 
 
 class NotificationListView(LoginRequiredMixin, ListView):
